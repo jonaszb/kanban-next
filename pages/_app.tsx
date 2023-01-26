b5e442fdc9c2c0debccbc104fcff61fd0852ff60
jonaszb <jonaszbil@gmail.com>
2023-01-26 13:14:54 +0000

    Comment out next-auth code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,18 @@ import type { AppProps } from 'next/app';
 import Layout from '../components/Layout/Layout';
 import ThemeContextProvider from '../store/ThemeContext';
 import BoardListContextProvider from '../store/BoardListContext';
-import { SessionProvider } from 'next-auth/react';
+// import { SessionProvider } from 'next-auth/react';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
-        <SessionProvider session={session}>
-            <ThemeContextProvider>
-                <BoardListContextProvider>
-                    <Layout>
-                        <Component {...pageProps} />
-                    </Layout>
-                </BoardListContextProvider>
-            </ThemeContextProvider>
-        </SessionProvider>
+        // <SessionProvider session={session}>
+        <ThemeContextProvider>
+            <BoardListContextProvider>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </BoardListContextProvider>
+        </ThemeContextProvider>
+        // </SessionProvider>
     );
 }
