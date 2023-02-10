7c504afae34d365a3e3b98eb8a4336b155810d79
jonaszb <jonaszbil@gmail.com>
2023-02-10 20:40:17 +0000

    Run tests with 3 workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ const config: PlaywrightTestConfig = {
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 1,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 4 : undefined,
+    workers: process.env.CI ? 3 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [['html', { open: 'never' }], ['list']],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
